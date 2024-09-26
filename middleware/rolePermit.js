0e7a91a4c86bd0313cab20c98b358b86bab32807
sabin <sabinshrestha292@gmail.com>
2024-09-26 23:16:51 +0545

    remove permission check for converstaion chat

--- middleware/rolePermit.js
+++ middleware/rolePermit.js
@@ -1,18 +1,17 @@
-const Role = require("../models/Role.js");
-const rolePermission = require("../helper/rolePermission");
+const Role = require('../models/Role.js');
+const rolePermission = require('../helper/rolePermission');
 const checkPermissions = (moduleName) => {
-    return async (req, res, next) => {
-        const roleName = (await Role.findById(req.user.role))?.name || 'user'
-        if (roleName === "admin") {
-            return next()
-        }
-        if (rolePermission[roleName].includes(moduleName)) {
-            next()
-        }
-        else {
-            console.log(roleName, moduleName)
-            return res.status(403).json({ message: "Not enough permission" });
-        }
-    };
+  return async (req, res, next) => {
+    const roleName = (await Role.findById(req.user.role))?.name || 'user';
+    if (roleName === 'admin') {
+      return next();
+    }
+    if (rolePermission[roleName].includes(moduleName)) {
+      next();
+    } else {
+      console.log(roleName, moduleName);
+      return res.status(403).json({message: 'Not enough permission'});
+    }
+  };
 };
 module.exports = checkPermissions;

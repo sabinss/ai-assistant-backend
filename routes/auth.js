8aab81438d886eb2925708c2c7b5fb3931ce1295
sabin <sabinshrestha292@gmail.com>
2024-11-22 08:25:54 +0545

    added organization token

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,19 @@
-const ctl = require("../controllers/authCtrl");
-
+const ctl = require('../controllers/authCtrl');
+console.log('process.env.APP_URL', process.env.APP_URL);
 module.exports = (app) => {
   app.post(`${process.env.APP_URL}/auth/signup`, ctl.signup);
   app.post(`${process.env.APP_URL}/auth/signin`, ctl.signin);
   app.post(`${process.env.APP_URL}/auth/changePassword`, ctl.changePassword);
   app.post(`${process.env.APP_URL}/auth/forgot-password`, ctl.forgotPassword);
-  app.post(`${process.env.APP_URL}/auth/verify-token`, ctl.verifyPasswordResetToken);
+  app.post(
+    `${process.env.APP_URL}/auth/verify-token`,
+    ctl.verifyPasswordResetToken
+  );
   app.post(`${process.env.APP_URL}/auth/email-verify`, ctl.verifyEmail);
-  app.post(`${process.env.APP_URL}/auth/sendEmailVerifyToken`, ctl.sendConfirmEmailToken);
-
+  app.post(
+    `${process.env.APP_URL}/auth/sendEmailVerifyToken`,
+    ctl.sendConfirmEmailToken
+  );
 
   // app.post(`${process.env.APP_URL}/auth/tokenRefresh`, ctl.tokenRefresh);
 };

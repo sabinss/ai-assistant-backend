1867a1a4aae6369a69a2e427ea3ae656097aeef3
sabin <sabinshrestha292@gmail.com>
2024-11-26 22:26:36 +0545

    added filter based on created date

--- controllers/feedbackCtrl.js
+++ controllers/feedbackCtrl.js
@@ -1,37 +1,37 @@
-const Feedback = require("../models/Feedback");
-const Conversation = require("../models/UserConversation");
-const Organization = require("../models/Organization");
-const FeedbackSurvey = require("../models/FeedbackSurvey");
-const http = require("../helper/http");
+const Feedback = require('../models/Feedback');
+const Conversation = require('../models/UserConversation');
+const Organization = require('../models/Organization');
+const FeedbackSurvey = require('../models/FeedbackSurvey');
+const http = require('../helper/http');
 
 exports.getFeedbacks = async (req, res) => {
   try {
     const page = parseInt(req?.query?.page) || 1;
     const limit = parseInt(req?.query?.limit) || 10;
     const startIndex = (page - 1) * limit;
-    const searchQuery = req?.query?.search || "";
-    const sortField = req?.query?.sortField || "createdAt";
-    const sortDirection = req?.query?.sortDirection === "desc" ? -1 : 1;
-    const { status, feedbackType, startDate, endDate } = req?.query;
+    const searchQuery = req?.query?.search || '';
+    const sortField = req?.query?.sortField || 'createdAt';
+    const sortDirection = req?.query?.sortDirection === 'desc' ? -1 : 1;
+    const {status, feedbackType, startDate, endDate} = req?.query;
     let searchCondition = {
       $and: [
-        { organization: req?.user?.organization },
+        {organization: req?.user?.organization},
         {
           $or: [
-            { "conversation.question": { $regex: searchQuery, $options: "i" } },
-            { "conversation.answer": { $regex: searchQuery, $options: "i" } },
-            { modified_answer: { $regex: searchQuery, $options: "i" } },
-            { feedback: { $regex: searchQuery, $options: "i" } },
+            {'conversation.question': {$regex: searchQuery, $options: 'i'}},
+            {'conversation.answer': {$regex: searchQuery, $options: 'i'}},
+            {modified_answer: {$regex: searchQuery, $options: 'i'}},
+            {feedback: {$regex: searchQuery, $options: 'i'}},
           ],
         },
       ],
     };
 
-    if (feedbackType && feedbackType !== "both") {
+    if (feedbackType && feedbackType !== 'both') {
       searchCondition.feedback = feedbackType;
     }
 
-    if (status && status !== "all") {
+    if (status && status !== 'all') {
       searchCondition.status = status;
     }
 
@@ -43,17 +43,17 @@ exports.getFeedbacks = async (req, res) => {
     }
 
     const feedbacks = await Feedback.find(searchCondition)
-      .populate("conversation")
+      .populate('conversation')
       .skip(startIndex)
       .limit(limit)
-      .sort({ [sortField]: sortDirection });
+      .sort({[sortField]: sortDirection});
 
     const totalCount = await Feedback.countDocuments(searchCondition);
     const totalPages = Math.ceil(totalCount / limit);
 
-    res.status(200).json({ feedbacks, totalPages, totalCount });
+    res.status(200).json({feedbacks, totalPages, totalCount});
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({message: 'Internal Server Error', error});
   }
 };
 
@@ -61,32 +61,31 @@ exports.getFeedback = async (req, res) => {
   const feedback_id = req.params.feedback_id;
   try {
     const feedback = await Feedback.findById(feedback_id)
-      .populate("conversation")
-      .populate("user");
-    if (!feedback)
-      return res.status(404).json({ message: "Feedback not found" });
-    res.status(200).json({ feedback });
+      .populate('conversation')
+      .populate('user');
+    if (!feedback) return res.status(404).json({message: 'Feedback not found'});
+    res.status(200).json({feedback});
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({message: 'Internal Server Error', error});
   }
 };
 
 exports.createFeedback = async (req, res) => {
-  const { conversation, feedback, modified_answer } = req.body;
+  const {conversation, feedback, modified_answer} = req.body;
   if (!conversation) {
-    return res.status(400).json({ message: "Missing required fields" });
+    return res.status(400).json({message: 'Missing required fields'});
   }
 
   try {
     const conversationData = await Conversation.findById(conversation);
     if (!conversationData) {
-      return res.status(404).json({ message: "Conversation not found" });
+      return res.status(404).json({message: 'Conversation not found'});
     }
     //get frequency from org
     const org = await Organization.findById(req?.user?.organization);
     const frequency = org?.temperature;
 
-    console.log("frequency from org is ", frequency);
+    console.log('frequency from org is ', frequency);
 
     const newFeedback = await Feedback.create({
       organization: req?.user?.organization,
@@ -94,28 +93,28 @@ exports.createFeedback = async (req, res) => {
       conversation,
       modified_answer,
       feedback,
-      status: "new",
+      status: 'new',
       frequency,
     });
 
     res
       .status(200)
-      .json({ message: "Feedback created successfully", newFeedback });
+      .json({message: 'Feedback created successfully', newFeedback});
   } catch (error) {
-    console.error("Error creating feedback:", error);
-    res.status(500).json({ message: "Internal server error", error });
+    console.error('Error creating feedback:', error);
+    res.status(500).json({message: 'Internal server error', error});
   }
 };
 
 exports.updateFeedback = async (req, res) => {
   try {
     const feedback_id = req.params.feedback_id;
-    const { feedback, modified_answer, status } = req.body;
+    const {feedback, modified_answer, status} = req.body;
 
     const updatedFeedback = await Feedback.findByIdAndUpdate(
       feedback_id,
-      { $set: { feedback, modified_answer, status: "updated" } },
-      { new: true }
+      {$set: {feedback, modified_answer, status: 'updated'}},
+      {new: true}
     );
 
     // Save Feedback to Assistant AI_API
@@ -126,69 +125,68 @@ exports.updateFeedback = async (req, res) => {
     );
 
     if (!updatedFeedback) {
-      return res.status(404).json({ message: "Feedback not found" });
+      return res.status(404).json({message: 'Feedback not found'});
     }
 
     res
       .status(200)
-      .json({ message: "Feedback updated", updatedFeedback, saveFeedback });
+      .json({message: 'Feedback updated', updatedFeedback, saveFeedback});
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({message: 'Internal Server Error', error});
   }
 };
 
 exports.deleteFeedback = async (req, res) => {
-  const { feedback_id } = req.params;
+  const {feedback_id} = req.params;
 
   try {
     const feedback = await Feedback.findByIdAndUpdate(
       feedback_id,
-      { $set: { status: "removed" } },
-      { new: true }
+      {$set: {status: 'removed'}},
+      {new: true}
     );
-    if (!feedback)
-      return res.status(404).json({ message: "Feedback not found" });
+    if (!feedback) return res.status(404).json({message: 'Feedback not found'});
 
-    res.status(200).json({ message: "Feedback deleted" });
+    res.status(200).json({message: 'Feedback deleted'});
   } catch (error) {
-    res.status(500).json({ message: "Internal server error", error });
+    res.status(500).json({message: 'Internal server error', error});
   }
 };
 exports.feedbackCounts = async (req, res) => {
-  const { startDate, endDate } = req?.query;
+  const {startDate, endDate} = req?.query;
   let searchCondition = {
     organization: req?.user?.organization,
-    createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) },
+    createdAt: {$gte: new Date(startDate), $lte: new Date(endDate)},
   };
   //count total doucments found  and then send
   try {
     const feedbackCounts = await Feedback.countDocuments(searchCondition);
     const likedFeedbackCounts = await Feedback.countDocuments({
       ...searchCondition,
-      feedback: "liked",
+      feedback: 'liked',
     });
     const dislikedFeedbackCounts = await Feedback.countDocuments({
       ...searchCondition,
-      feedback: "disliked",
+      feedback: 'disliked',
     });
     res
       .status(200)
-      .json({ feedbackCounts, likedFeedbackCounts, dislikedFeedbackCounts });
+      .json({feedbackCounts, likedFeedbackCounts, dislikedFeedbackCounts});
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({message: 'Internal Server Error', error});
   }
 };
 exports.createPublicFeedback = async (req, res) => {
-  const { conversation, feedback, modified_answer, org_id, user_id } = req.body;
+  const {conversation, feedback, modified_answer, org_id, user_id} = req.body;
   if (!conversation) {
-    return res.status(400).json({ message: "Missing required fields" });
+    return res.status(400).json({message: 'Missing required fields'});
   }
 
   try {
     const conversationData = await Conversation.findById(conversation);
     if (!conversationData) {
-      return res.status(404).json({ message: "Conversation not found" });
+      return res.status(404).json({message: 'Conversation not found'});
     }
     //get frequency from org
     const org = await Organization.findById(org_id);
@@ -200,16 +198,16 @@ exports.createPublicFeedback = async (req, res) => {
       conversation,
       modified_answer,
       feedback,
-      status: "new",
+      status: 'new',
       frequency,
     });
 
     res
       .status(200)
-      .json({ message: "Feedback created successfully", newFeedback });
+      .json({message: 'Feedback created successfully', newFeedback});
   } catch (error) {
-    console.error("Error creating feedback:", error);
-    res.status(500).json({ message: "Internal server error", error });
+    console.error('Error creating feedback:', error);
+    res.status(500).json({message: 'Internal server error', error});
   }
 };
 
@@ -245,7 +243,8 @@ exports.createFeedbackSurvey = async (req, res) => {
 exports.getFeedbackSurveys = async (req, res) => {
   try {
     // Extract query parameters
-    const {organization_id, customer_id, user_id, updated_date} = req.query;
+    const {organization_id, customer_id, user_id, updated_date, created_date} =
+      req.query;
     console.log('organization_id', organization_id);
     // Build the filter object
     const filter = {};
@@ -256,6 +255,11 @@ exports.getFeedbackSurveys = async (req, res) => {
       const filterDate = new Date(updated_date);
       filterDate.setHours(0, 0, 0, 0); // Ensure it starts from midnight
       filter['updatedAt'] = {$gt: filterDate};
+    }
+    if (created_date) {
+      const filterDate = new Date(created_date);
+      filterDate.setHours(0, 0, 0, 0); // Ensure it starts from midnight
+      searchCondition['createdAt'] = {$gt: filterDate};
     }
     if (customer_id) {
       filter.customer = customer_id;

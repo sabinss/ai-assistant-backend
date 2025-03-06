6f2ee32cca306ef69b1fff9d3e947df2fa27053e
sabin <sabin@noveltytechnology.com>
2025-03-06 07:53:14 +0545

    testing pushing

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpecs = require("./routes/swagger");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./routes/swagger');
 const port = process.env.PORT || 8000;
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
 
 //todo chat message sort by created date aila sorting milara ako chainclear
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-const db = require("./helper/db");
-const { googleOauthHandler } = require("./controllers/session.controller");
-const seedTaskAgentForOrganization = require("./seeders/taskAgentSeeder");
-const updateOrganizationForAll = require("./seeders/customerLoginDetailUpdateOrgId");
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+const db = require('./helper/db');
+const { googleOauthHandler } = require('./controllers/session.controller');
+const seedTaskAgentForOrganization = require('./seeders/taskAgentSeeder');
+const updateOrganizationForAll = require('./seeders/customerLoginDetailUpdateOrgId');
 // const seedCustomerFeature = require('./seeders/seedCustomerFeature');
 // const seedPromptInAllOrganization = require('./seeders/promptSeed');
 // const bulkInsertNotifications = require("./seeders/seedNotificationData");
@@ -23,7 +22,7 @@ app.use(express.json());
 // seedTaskAgentForOrganization();
 // bulkInsertNotifications();
 const corsOptions = {
-  origin: "*",
+  origin: '*',
   credentials: true,
 };
 // seedCustomerFeature();
@@ -33,21 +32,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.options('*', cors(corsOptions)); // Preflight request support
 
 http: db.connect();
-app.get("/health-check", async (req, res) => {
+app.get('/health-check', async (req, res) => {
   const check = await db.connect();
   if (!check) {
-    res.status(500).send("Not OK");
+    res.status(500).send('Not OK');
   }
-  res.status(200).send("Instwise API running..");
+  res.status(200).send('Instwise API running..');
 });
 //seeding line is this
 //require("./seeders/allseeder.js")()
 
-app.get("/api/sessions/oauth/google", googleOauthHandler);
+app.get('/api/sessions/oauth/google', googleOauthHandler);
 
-require("./service/userAuth");
-require("./models");
-require("./routes")(app);
+require('./service/userAuth');
+require('./models');
+require('./routes')(app);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);

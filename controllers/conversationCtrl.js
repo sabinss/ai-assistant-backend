ac3430db854d749bfdd0c9c9a94f6a0b021d66da
sabin <sabin@noveltytechnology.com>
2025-03-16 09:16:31 +0545

    route change from publi/ask to ask/public

--- controllers/conversationCtrl.js
+++ controllers/conversationCtrl.js
@@ -336,7 +336,7 @@ exports.addPublicConversation = async (req, res) => {
   const { org_id, chat_session, user_email = null } = req.query;
   try {
     const { question, user_email, customer_id } = req.body;
-    let url = `http://3.17.138.140:8000/public/ask?query=${encodeURIComponent(
+    let url = `http://3.17.138.140:8000/ask/public?query=${encodeURIComponent(
       question
     )}&user_email=${user_email}&org_id=${org_id}&customer_id=null`;
     console.log('url', url);

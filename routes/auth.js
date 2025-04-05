be3bf8350e6a88397cb8223d2dea7d0dd6e70bb4
sabin <sabin@noveltytechnology.com>
2025-04-05 21:04:26 +0545

    master backup

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const ctl = require('../controllers/authCtrl');
 const authUser = require('../middleware/authUser');
-console.log('process.env.APP_URL', process.env.APP_URL);
+
 module.exports = (app) => {
   app.post(`${process.env.APP_URL}/auth/signup`, ctl.signup);
   app.post(`${process.env.APP_URL}/auth/signin`, ctl.signin);

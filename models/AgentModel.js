6b08f6a875ab71adfaf992c498681ffd64953d19
sabin <sabin@noveltytechnology.com>
2025-08-21 22:21:10 +0545

    batch size

--- models/AgentModel.js
+++ models/AgentModel.js
@@ -1,46 +1,58 @@
 const mongoose = require('mongoose');
 
 const agentModel = new mongoose.Schema(
-    {
-        name: { type: String, default: null },
-        objective: { type: String, default: null },
-        routing_instruction: { type: String },
-        greeting: { type: String },
-        routing_examples: { type: String },
-        tools_used: {
-            type: String
-        },
-        primary_instruction: {
-            type: String
-        },
-        isAgent: {
-            type: Boolean,
-            default: false
-        },
-        frequency: {
-            type: String
-        },
-        dayTime: {
-            type: String
-        },
-        organization: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Organization',
-            required: true
-        },
-        active: {
-            type: Boolean
-        },
-        agentInstructions: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'AgentTask' // Reference to AgentTask
-            }
-        ]
+  {
+    name: { type: String, default: null },
+    objective: { type: String, default: null },
+    routing_instruction: { type: String },
+    greeting: { type: String },
+    routing_examples: { type: String },
+    tools_used: {
+      type: String,
     },
-    {
-        timestamps: true
-    }
+    primary_instruction: {
+      type: String,
+    },
+    isAgent: {
+      type: Boolean,
+      default: false,
+    },
+    frequency: {
+      type: String,
+    },
+    dayTime: {
+      type: String,
+    },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Organization',
+      required: true,
+    },
+    active: {
+      type: Boolean,
+    },
+    agentInstructions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'AgentTask', // Reference to AgentTask
+      },
+    ],
+    batch_process_enabled: {
+      type: Boolean,
+      default: false,
+    },
+    batch_scope: {
+      type: String,
+      default: null,
+    },
+    batch_size: {
+      type: Number,
+      default: 1,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model('Agent', agentModel);

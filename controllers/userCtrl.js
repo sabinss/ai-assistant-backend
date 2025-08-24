c9a191e7e2170265d6754eaa09471c3f9186342f
sabin <sabin@noveltytechnology.com>
2025-08-24 22:14:24 +0545

    customer check

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,16 +1,16 @@
-const { log } = require("console");
-const User = require("../models/User");
-const bcrypt = require("bcrypt")
-const Role = require("../models/Role");
-const Status = require("../models/Status");
+const { log } = require('console');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const Role = require('../models/Role');
+const Status = require('../models/Status');
 exports.getUsers = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const startIndex = (page - 1) * limit;
     const searchQuery = req.query.search || '';
-    const sortField = req.query.sortField || "email";
-    const sortDirection = req.query.sortDirection === "desc" ? -1 : 1;
+    const sortField = req.query.sortField || 'email';
+    const sortDirection = req.query.sortDirection === 'desc' ? -1 : 1;
 
     const searchCondition = {
       $and: [
@@ -19,50 +19,57 @@ exports.getUsers = async (req, res) => {
           $or: [
             { email: { $regex: searchQuery, $options: 'i' } },
             { first_name: { $regex: searchQuery, $options: 'i' } },
-            { last_name: { $regex: searchQuery, $options: 'i' } }
-          ]
-        }
-      ]
+            { last_name: { $regex: searchQuery, $options: 'i' } },
+          ],
+        },
+      ],
     };
     const users = await User.find(searchCondition)
-      .select("-password")
+      .select('-password')
       .skip(startIndex)
       .limit(limit)
       .sort({ [sortField]: sortDirection })
       .populate('role') // Populate the status field
       .populate('status');
 
-
     const totalUsers = await User.countDocuments(searchCondition);
     const totalPages = Math.ceil(totalUsers / limit);
     res.status(200).json({ users, totalPages });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
-
-
-
 
 exports.getUser = async (req, res) => {
   const user_id = req.params.user_id;
   try {
-    const user = await User.findById(user_id).select("-password").populate('role').populate('status');
-    if (!user) return res.status(404).json({ message: "User not found" });
+    const user = await User.findById(user_id)
+      .select('-password')
+      .populate('role')
+      .populate('status');
+    if (!user) return res.status(404).json({ message: 'User not found' });
     res.status(200).json({ user });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
 exports.update = async (req, res) => {
   try {
     const user_id = req.params.user_id;
-    const { first_name, last_name, email, role, newPassword, status, currentPassword } = req.body;
+    const {
+      first_name,
+      last_name,
+      email,
+      role,
+      newPassword,
+      status,
+      currentPassword,
+    } = req.body;
     let data;
     let user = await User.findById(user_id);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: 'User not found' });
     }
 
     if (currentPassword) {
@@ -72,14 +79,20 @@ exports.update = async (req, res) => {
       );
 
       if (!isCurrentPassword) {
-        return res.status(403).json({ message: "Incorrect current password" });
+        return res.status(403).json({ message: 'Incorrect current password' });
       }
     }
     // add logic to only change new password if it is organization here.
     if (newPassword) {
-      data = { first_name, last_name, email, role, status, password: bcrypt.hashSync(newPassword, 10) };
-    }
-    else {
+      data = {
+        first_name,
+        last_name,
+        email,
+        role,
+        status,
+        password: bcrypt.hashSync(newPassword, 10),
+      };
+    } else {
       data = { first_name, last_name, email, role, status };
     }
 
@@ -90,10 +103,10 @@ exports.update = async (req, res) => {
         new: true,
       }
     );
-    res.status(200).json({ message: "User updated" });
+    res.status(200).json({ message: 'User updated' });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
@@ -101,20 +114,19 @@ exports.deleteUser = async (req, res) => {
   const user_id = req.params.user_id;
   try {
     if (user_id === req?.user?._id.toString()) {
-      return res.status(201).json({ message: "User is currently logged in" })
+      return res.status(201).json({ message: 'User is currently logged in' });
     }
     const user = await User.findByIdAndDelete(user_id);
-    return res.status(200).json({ message: "User deleted" });
-  }
-  catch (error) {
-    res.status(500).json({ message: "Internal server error", error });
+    return res.status(200).json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error });
   }
 };
 
 exports.addUser = async (req, res) => {
   const { first_name, last_name, email, role, status, password } = req.body;
   if (!first_name || !last_name || !email) {
-    return res.status(400).json({ message: "Missing required fields" });
+    return res.status(400).json({ message: 'Missing required fields' });
   }
   const checkExistUser = await User.findOne({ email: email });
   if (checkExistUser?.email) {
@@ -122,8 +134,8 @@ exports.addUser = async (req, res) => {
   }
   const hashed_password = bcrypt.hashSync(password, 10);
   //set default role and status to admin and pending resp
-  const activeStatus = await Status.findOne({ name: "active" });
-  const adminRole = await Role.findOne({ name: "admin" });
+  const activeStatus = await Status.findOne({ name: 'active' });
+  const adminRole = await Role.findOne({ name: 'admin' });
 
   try {
     const user = await User.create({
@@ -133,24 +145,31 @@ exports.addUser = async (req, res) => {
       organization: req?.user?.organization,
       role: role ? role : adminRole,
       status: activeStatus,
-      password: hashed_password
+      password: hashed_password,
     });
 
-    res.status(200).json({ message: "User created successfully", user });
+    res.status(200).json({ message: 'User created successfully', user });
   } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ message: "Internal server error", error });
+    console.error('Error creating user:', error);
+    res.status(500).json({ message: 'Internal server error', error });
   }
 };
 exports.updateProfile = async (req, res) => {
   try {
     const user_id = req.user._id;
 
-    const { first_name, last_name, email, newPassword, status, currentPassword } = req.body;
+    const {
+      first_name,
+      last_name,
+      email,
+      newPassword,
+      status,
+      currentPassword,
+    } = req.body;
     let data;
     let user = await User.findById(user_id);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: 'User not found' });
     }
 
     if (currentPassword) {
@@ -160,14 +179,19 @@ exports.updateProfile = async (req, res) => {
       );
 
       if (!isCurrentPassword) {
-        return res.status(403).json({ message: "Incorrect current password" });
+        return res.status(403).json({ message: 'Incorrect current password' });
       }
     }
     // add logic to only change new password if it is organization here.
     if (newPassword) {
-      data = { first_name, last_name, email, status, password: bcrypt.hashSync(newPassword, 10) };
-    }
-    else {
+      data = {
+        first_name,
+        last_name,
+        email,
+        status,
+        password: bcrypt.hashSync(newPassword, 10),
+      };
+    } else {
       data = { first_name, last_name, email, status };
     }
 
@@ -178,21 +202,24 @@ exports.updateProfile = async (req, res) => {
         new: true,
       }
     );
-    res.status(200).json({ message: "User updated" });
+    res.status(200).json({ message: 'User updated' });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 exports.getProfile = async (req, res) => {
-  console.log(req.user)
-  const user_id = req.user._id;
+  console.log(req.user);
+  const user_id = req.user?._id || req.user?.user_id;
   try {
-    const user = await User.findById(user_id).select("-password").populate('role').populate('status');
-    if (!user) return res.status(404).json({ message: "User not found" });
+    const user = await User.findById(user_id)
+      .select('-password')
+      .populate('role')
+      .populate('status');
+    if (!user) return res.status(404).json({ message: 'User not found' });
     res.status(200).json({ user });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error });
+    res.status(500).json({ message: 'Internal Server Error', error });
   }
 };
 
@@ -201,17 +228,17 @@ exports.changeSession = async (req, res) => {
   const user_id = req.user._id;
   try {
     const user = await User.findById(user_id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: 'User not found' });
     const updatedUser = await User.findByIdAndUpdate(
       user_id,
       { chatSession: newSession },
       { new: true }
     );
-    res.status(200).json({ message: "Session changed successfully", newSession });
-  }
-  catch (error) {
+    res
+      .status(200)
+      .json({ message: 'Session changed successfully', newSession });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
-

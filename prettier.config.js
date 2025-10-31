c55c338221cc319b0cbfd7d23d079c18b182839a
Suvash Joshi <champsuvash@gmail.com>
2025-10-31 05:10:07 +0000

    chore: formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,36 @@
 // prettier.config.js
 module.exports = {
-  trailingComma: "es5",
+  // Line length
+  printWidth: 100,
+
+  // Indentation
   tabWidth: 2,
+  useTabs: false,
+
+  // Semicolons
   semi: true,
+
+  // Quotes
   singleQuote: false,
-  printWidth: 80,
+  quoteProps: "as-needed",
+
+  // Trailing commas
+  trailingComma: "es5",
+
+  // Spacing
+  bracketSpacing: true,
+  bracketSameLine: false,
   arrowParens: "always",
+
+  // Line endings
   endOfLine: "lf",
+
+  // HTML/JSX formatting
+  htmlWhitespaceSensitivity: "css",
+
+  // Embedded languages
+  embeddedLanguageFormatting: "auto",
+
+  // Prose wrapping
+  proseWrap: "preserve",
 };

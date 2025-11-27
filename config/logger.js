a8304b3713b9838cce57d2928f622b5a80f47e25
sabin <sabin@noveltytechnology.com>
2025-11-27 21:38:04 +0545

    remove logs transport

--- config/logger.js
+++ config/logger.js
@@ -1,18 +1,18 @@
 // logger.js
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, printf } = format;
 
 const logFormat = printf(({ level, message, timestamp }) => {
-    return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+  return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
 });
 
 const logger = createLogger({
-    level: 'info',
-    format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
-    transports: [
-        new transports.File({ filename: 'logs/cron.log' }), // ensure `logs/` folder exists
-        new transports.File({ filename: 'logs/error.log', level: 'error' })
-    ]
+  level: "info",
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
+  transports: [
+    // new transports.File({ filename: 'logs/cron.log' }), // ensure `logs/` folder exists
+    // new transports.File({ filename: 'logs/error.log', level: 'error' })
+  ],
 });
 
 module.exports = logger;
